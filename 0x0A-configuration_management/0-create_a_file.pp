4ba1b4032c2fcc352d4a49a34816cb11f4bc4b49
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-21 20:25:07 +0100

    debugged error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Using Puppet, create a file in /tmp
+# Used Puppet to create a file in /tmp
 
 file { '/tmp/school':
   ensure  => 'file',
-  content => 'I love puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love puppet',
 }
 
