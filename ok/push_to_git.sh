49a378a543459f38726c4a53188b71678fd74620
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-12 11:43:53 +0100

    MY commit message

--- ok/push_to_git.sh
+++ ok/push_to_git.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-git add "$1"
-git commit -m "$2"
-git push
+# check if at least one file and commit message is provided
+if [[ $# -lt 2 ]]; then
+  echo "Usage: $0 <file1> [<file2> ...] <commit message>"
+  exit 1
+fi
+
+# add each file to git
+for (( i=1; i<=$#-1; i++ )); do
+  git add "${!i}"
+done
+
+# commit changes with provided message
+commit_msg="${!#}"
+git commit -m "$commit_msg"
+
+#git push
