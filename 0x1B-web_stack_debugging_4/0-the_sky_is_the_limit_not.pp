33390993caad131ff940ffd289200e34af2abc72
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-06-15 12:54:01 +0100

    fixed

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# fixing nginx to accept and serve more requests
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restarting Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
